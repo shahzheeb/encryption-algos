dc04ca47a1c6f812e1365f525fa55c5b2a6bee99
shahzheeb khan <shahzheeb@gmail.com>
2022-01-14 23:40:03 -0500

    initial commit

---
+++ src/test/java/org/shahzheeb/AESCBCPassowordEncryptionTest.java
@@ -0,0 +1,20 @@
+package org.shahzheeb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AESCBCPassowordEncryptionTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
